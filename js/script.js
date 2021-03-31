0753e3ce68297447813229af110f6cfafccc4faf
Alejandro Jimenez Rios <fxm.sicg2021@gmail.com>
2021-03-31 16:24:08 -0600

    Added Planilla, individual pages, opacity to links and start of Propuestas

--- js/script.js
+++ js/script.js
@@ -1,3 +1,4 @@
+// Swiper script
 var swiper = new Swiper('.swiper-container', {
     slidesPerView: 1,
     spaceBetween: 1,
@@ -37,3 +38,19 @@ var swiper = new Swiper('.swiper-container', {
       prevEl: '.swiper-button-prev',
     },
 });
+
+// Collapsible script
+var coll = document.getElementsByClassName("collapsible");
+var i;
+
+for (i = 0; i < coll.length; i++) {
+  coll[i].addEventListener("click", function() {
+    this.classList.toggle("active");
+    var content = this.nextElementSibling;
+    if (content.style.display === "block") {
+      content.style.display = "none";
+    } else {
+      content.style.display = "block";
+    }
+  });
+}
